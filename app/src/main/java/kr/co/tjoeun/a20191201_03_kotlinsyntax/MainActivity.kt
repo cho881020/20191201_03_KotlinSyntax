7284f1616691b1b1db5952891917f11817436a69
조경진 <kj_cho@idealidea.co.kr>
2019-12-01 15:59:45 +0900

    코틀린에서의 기본 for문 연습  - 배열안에 있는 데이터를 하나하나 뽑아서 사용

--- app/src/main/java/kr/co/tjoeun/a20191201_03_kotlinsyntax/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191201_03_kotlinsyntax/MainActivity.kt
@@ -50,6 +50,12 @@ class MainActivity : AppCompatActivity() {
 
         userList.add(user2)
 
+        val user4 = User()
+        user4.name = "김미현"
+        user4.loginId = "student2"
+
+        userList.add(user4)
+
         Log.d("들어있는사람수", "${userList.size}명")
         Log.d("1번칸에 있는 사람의 이름", "${userList.get(1).name}")
 
@@ -59,9 +65,27 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("조경진이 있는 위치", "${userList.indexOf(user3)}")
 
-        userList.remove(user3)
+//        userList.remove(user3)
+//
+//        Log.d("들어있는사람수", "${userList.size}명")
 
-        Log.d("들어있는사람수", "${userList.size}명")
+
+//        var count = 0
+//        while (true) {
+//
+//            count++
+//
+//            if (count >= 10) {
+//                break
+//            }
+//        }
+
+//          사용자 목록에 들어있는 사람들의 이름 / 아이디 출력
+
+        for (user in userList) {
+            Log.d("사람이름/아이디", "${user.name} / ${user.loginId}")
+        }
+
     }
 }
 
