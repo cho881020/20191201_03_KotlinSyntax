d49ba5f2ae8831340c8093e17b995e9db52f90b1
조경진 <kj_cho@idealidea.co.kr>
2019-12-01 15:52:05 +0900

    내가만든 클래스의 ArrayList 연습  - User클래스의 equals 함수를 오버라이딩

--- app/src/main/java/kr/co/tjoeun/a20191201_03_kotlinsyntax/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191201_03_kotlinsyntax/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.a20191201_03_kotlinsyntax
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kr.co.tjoeun.a20191201_03_kotlinsyntax.datas.User
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +32,46 @@ class MainActivity : AppCompatActivity() {
 //        맨 앞자료 삭제하고싶다.
         lottoNumArr.removeAt(0)
 
+
+
+
+
+        val userList = ArrayList<User>()
+
+        val user1 = User()
+        user1.name = "조경진"
+        user1.loginId = "cho881020"
+
+        userList.add(user1)
+
+        val user2 = User()
+        user2.name = "계석준"
+        user2.loginId = "student1"
+
+        userList.add(user2)
+
+        Log.d("들어있는사람수", "${userList.size}명")
+        Log.d("1번칸에 있는 사람의 이름", "${userList.get(1).name}")
+
+        val user3 = User()
+        user3.name = "조경진"
+        user3.loginId = "cho881020"
+
+        Log.d("조경진이 있는 위치", "${userList.indexOf(user3)}")
+
+        userList.remove(user3)
+
+        Log.d("들어있는사람수", "${userList.size}명")
     }
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/kr/co/tjoeun/a20191201_03_kotlinsyntax/datas/User.kt
@@ -0,0 +1,14 @@
+package kr.co.tjoeun.a20191201_03_kotlinsyntax.datas
+
+class User {
+
+    var name = ""
+    var loginId = ""
+
+    override fun equals(other: Any?): Boolean {
+        val otherUser = other as User
+
+        return this.name == otherUser.name && this.loginId == otherUser.loginId
+    }
+
+}
